9db825de01cdc1021145cafb127cf50ea5211687
harpinner <harpinner@users.noreply.github.com>
2026-07-02 18:27:53 -0400

    Put In placeholder home page, added database credentials, and started implementing site structure

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require('express');
+import express from 'express';
+import ejs from 'ejs';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+   
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+ // res.send('Hello World');
+ res.render('placeholder', { title: 'Home' });
 });
 
 app.listen(PORT, () => {
